b350db0f1fea71002b467a4f77bf8ffac1ebb6cb
SuperEwald <public@superewald.net>
2021-01-22 11:13:08 +0100

    added missing files after reorganize

---
+++ Launcher/Config/Config.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+namespace BLRevive.Launcher
+{
+    /// <summary>
+    /// Provides read/write access to JSON configuration.
+    /// </summary>
+    public class Config
+    {
+        
+        public const string DefaultPlayerName = "Player";
+        
+        private const string DefaultLocalHostIP = "127.0.0.1";
+        private const string DefaultLocalHostPort = "7777";
+        public static Server DefaultLocalHostServer = new Server()
+        {
+            Address = DefaultLocalHostIP, 
+            Port = DefaultLocalHostPort
+        };
+
+        public const int MaxClientHostListSize = 50;
+
+        /// <summary>
+        /// The log level to use
+        /// </summary>
+        public int LogLevel;
+
+        /// <summary>
+        /// Delay between server and client startup.
+        /// </summary>
+        public int ServerStartupOffset;
+
+        /// <summary>
+        /// Saved username
+        /// </summary>
+        public string Username;
+
+        /// <summary>
+        /// Contains the server connected to in the previous session.
+        /// </summary>
+        public PreviousHost PreviousHost;
+
+        /// <summary>
+        /// Available Maps.
+        /// </summary>
+        public string[] Maps;
+
+        /// <summary>
+        /// Available Gamemodes.
+        /// </summary>
+        public string[] Gamemodes;
+
+        /// <summary>
+        /// Available Playlists.
+        /// </summary>
+        public string[] Playlists;
+
+        /// <summary>
+        /// Known Host Servers by IP or Name and port  
+        /// </summary>
+        public List<Server> Hosts;
+
+        /// <summary>
+        /// path to blacklight installation directory
+        /// </summary>
+        public string GameFolder;
+
+        private static Config _Config = null;
+        private const string LauncherConfigFileName = "LauncherConfig.json";
+
+        /// <summary>
+        /// Get the configuration from JSON.
+        /// </summary>
+        /// <returns>Instance of this class with parsed config.</returns>
+        public static Config Get()
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+            };
+
+            try
+            {
+                if (_Config == null)
+                    _Config = JsonSerializer.Deserialize<Config>(File.ReadAllText(LauncherConfigFileName), serializerOptions);
+            }
+            catch
+            {
+                MessageBox.Avalonia.MessageBoxManager.
+                GetMessageBoxStandardWindow("Error", $"Failed to parse {LauncherConfigFileName}!")
+                .Show();
+                //Log.Debug(ex.Message);
+
+                Environment.Exit(2121800001);
+            }
+
+
+            return _Config;
+        }
+
+        /// <summary>
+        /// Save the current config to JSON.
+        /// </summary>
+        /// <returns>Whether saving succeeded.</returns>
+        public static bool Save()
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            try
+            {
+                string jsonConfig = JsonSerializer.Serialize(Get(), serializerOptions);
+                File.WriteAllText(LauncherConfigFileName, jsonConfig);
+            } catch (Exception ex)
+            {
+                MessageBox.Avalonia.MessageBoxManager.
+                GetMessageBoxStandardWindow("Error", $"Error writing config: {ex.Message}")
+                .Show();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Launcher/Config/HostsConfig.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+namespace BLRevive.Launcher
+{
+    /// <summary>
+    /// Provides read/write access to JSON hosts backup configuration.
+    /// </summary>
+    public class HostsConfig
+    {
+        /// <summary>
+        /// Known Hosts by IP or Name and port
+        /// </summary>
+        public List<Server> Hosts;
+
+        private static HostsConfig _HostsConfig = null;
+        public const string HostsConfigFileName = "HostsConfigBackup.json";
+
+        /// <summary>
+        /// Get the host configuration backup from JSON.
+        /// </summary>
+        /// <returns>Instance of this class with parsed host config backup</returns>
+        public static HostsConfig Get()
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+            };
+
+            try
+            {
+                if (File.Exists(HostsConfigFileName))
+                {
+                    _HostsConfig = JsonSerializer.Deserialize<HostsConfig>(File.ReadAllText(HostsConfigFileName), serializerOptions);
+                }
+                else
+                {
+                    MessageBox.Avalonia.MessageBoxManager.
+                    GetMessageBoxStandardWindow("Error", $"Failed to parse {HostsConfigFileName}, there is no backup available!")
+                    .Show();
+                }
+            }
+            catch
+            {
+                MessageBox.Avalonia.MessageBoxManager.
+                GetMessageBoxStandardWindow("Error", $"Failed to parse {HostsConfigFileName}!")
+                .Show();
+            }
+
+            return _HostsConfig ?? (_HostsConfig = new HostsConfig());
+        }
+
+        /// <summary>
+        /// Saves the current hosts to the dedicated hosts config backup JSON
+        /// 1- deletes the old backup JSON config if already exists
+        /// 2- creates new backup JSON file to store the hosts 
+        /// </summary>
+        /// <returns>whether saving succeeded</returns>
+        public static bool Save()
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            try
+            {
+                List<Server> currentHosts = Config.Get().Hosts;
+                if (currentHosts == null || currentHosts.Count == 0)
+                {
+                    MessageBox.Avalonia.MessageBoxManager.
+                    GetMessageBoxStandardWindow("Error", "There are no hosts servers available to save!")
+                    .Show();
+
+                    return false;
+                }
+
+                if (File.Exists(HostsConfigFileName))
+                {
+                    File.Delete(HostsConfigFileName);
+                }
+                File.Create(HostsConfigFileName).Close();
+
+                _HostsConfig = new HostsConfig { Hosts = currentHosts };
+
+                string jsonConfig = JsonSerializer.Serialize(_HostsConfig, serializerOptions);
+                File.WriteAllText(HostsConfigFileName, jsonConfig);
+            } catch (Exception ex)
+            {                    
+                MessageBox.Avalonia.MessageBoxManager.
+                GetMessageBoxStandardWindow("Error", $"Error on writing hosts config backup: {ex.Message}")
+                .Show();
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
